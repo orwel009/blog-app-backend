2692154543d1c6bca4d9c7f76441d5dfcc4c1edc
orwel009 <orwelvincent1@gmail.com>
2024-06-13 16:19:30 +0530

    view complete

--- app.js
+++ app.js
@@ -58,6 +58,26 @@ app.post("/signin",(req,res)=>{
   )  
 })
 
+app.post("/view",(req,res)=>{
+    let token = req.headers["token"]
+    jwt.verify(token,"blog-app",
+        (error,decoded)=>{
+            if (error) {
+                res.json({"status":"unathorized access"})
+            } else {
+                if(decoded){
+                    userModel.find().then(
+                        (response)=>{
+                            res.json(response)
+                        }
+                    ).catch()
+                }
+            }
+        }
+    )
+    
+})
+
 app.listen(8080,()=>{
     console.log("Server Started")
 })
